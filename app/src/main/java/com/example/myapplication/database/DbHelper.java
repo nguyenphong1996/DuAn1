a0c3eab5960bafea4263c8876da9689342bccf35
LittleTUES <trinhquoctien26@gmail.com>
2023-11-28 19:34:28 +0700

    database + model

--- app/src/main/java/com/example/myapplication/database/DbHelper.java
+++ app/src/main/java/com/example/myapplication/database/DbHelper.java
@@ -11,28 +11,41 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Bảng người dùng
-        db.execSQL("CREATE TABLE NguoiDung(" +
-                "Email text primary key," +
-                "Password text," +
-                "FullName text)"
+        // Bảng tài khoản người dùng
+        db.execSQL("CREATE TABLE TaiKhoanND(" +
+                "Id_TaiKhoan integer PRIMARY KEY autoincrement," +
+                "Email text ," +
+                "MatKhau text," +
+                "TenDangNhap text)"
         );
-        String data = "INSERT INTO User VALUES" +
-                "('tientq@fpt.edu.vn', 'ps27928', 'Trịnh Quốc Tiến')," +
-                "('quyls@fpt.edu.vn', 'ps27683', 'Lê Sỹ Quý')";
+        String data = "INSERT INTO TaiKhoanND VALUES" +
+                "(0, 'tientq@fpt.edu.vn', 'ps27928', 'Trịnh Quốc Tiến')," +
+                "(1, 'quyls@fpt.edu.vn', 'ps27683', 'Lê Sỹ Quý')";
         db.execSQL(data);
 
-        // Bảng sản phẩm
+        // Bảng thông tin người dùng
+        db.execSQL("CREATE TABLE ThongTinND(" +
+                "Id_NguoiDung INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "SDT text," +
+                "Role_ND INTEGER," +
+                "Id_TaiKhoan INTEGER," +
+                "FOREIGN KEY(Id_TaiKhoan) REFERENCES TaiKhoanND(Id_TaiKhoan))"
+        );
+        data = "INSERT INTO ThongTinND VALUES" +
+                "(0, 'tientq@fpt.edu.vn', 'ps27928', 'Trịnh Quốc Tiến')," +
+                "(1, 'quyls@fpt.edu.vn', 'ps27683', 'Lê Sỹ Quý')";
+        db.execSQL(data);
+
+        // Bảng dịch vụ
         db.execSQL("CREATE TABLE DichVu(" +
-                "Id integer primary key autoincrement," +
-                "Name text," +
-                "Price integer," +
-                "Quantity integer," +
-                "Is_buy integer)"
+                "Id_DV integer primary key autoincrement," +
+                "Ten_DV text," +
+                "GiaTien float," +
+                "MoTa text)"
         );
         data = "INSERT INTO Item VALUES" +
-                "(null, 'Shedu Set', 70, 1, '1')," +
-                "(null, 'Garuda Prime Set', 55, 1, '0')";
+                "(null, 'Giặt sấy đồ < 3kg', 45000, null)," +
+                "(null, 'Giặt sấy đồ < 5kg', 75000, null)";
         db.execSQL(data);
     }
 
